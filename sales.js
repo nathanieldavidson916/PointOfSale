6c0f4bb2c72a0af09d97f1b51d693a7c6cf9b47a
Nathan Davidson <ndavidson4@mail.csuchico.edu>
2016-10-31 16:14:09 -0700

    completed addItem()

--- sales.js
+++ sales.js
@@ -5,10 +5,18 @@ function addItem()
   var newItem;
   newItem = document.getElementById("price").value;
 
-  if(isNaN(newItem) == true)
-    window.alert("Enter price as a number");
   //IF newItem is not a number
   // THEN show an alert: "Enter price as a number"
+  if(isNaN(newItem) == true)
+    window.alert("Enter price as a number");
+  else{
+    newItem = Number(newItem);
+    runningTotal += newItem;
+    var dollars = asCurrency(runningTotal);
+    document.getElementById("subtotal").innerHTML = dollars;
+    document.getElementById("price").value = "";
+    setCookie("preTax", runningTotal, 80);
+  }
   //OTHERWISE,
   // update newItem to its value cast as a number
   // update runningTotal to be its value plus newItem
